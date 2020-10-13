8a8ed43b16663ba957ae5497ce6ca4b6ae4b1018
vaibhav <48256562+rautvaibhav56@users.noreply.github.com>
2020-10-13 18:11:00 +0530

    Add files via upload

---
+++ ArraysInJava/src/demo/MultidimensionalArray.java
@@ -0,0 +1,20 @@
+package demo;
+//2D Array
+public class MultidimensionalArray {
+
+	public static void main(String[] args) {
+	
+		//2D array  
+		int arr[][]={{1,2,3},{3,4,5},{1,4,5}};  
+		//printing 2D array  
+		for(int i=0;i<3;i++){  
+		 for(int j=0;j<3;j++){  
+		   System.out.print(arr[i][j]+" ");  
+		 }  
+		 System.out.println();  
+		}  
+		
+		
+	}
+
+}
